5121064d4bdd0027796b6148a1c676ba4b5cfe6e
Igor Zinovyev <zinigor@gmail.com>
2026-01-19 16:47:01 +0300

    Enabled Doctrine package Phan scanning.

---
+++ .phan/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Phan configuration for DDD package.
+ *
+ * @package tangible/ddd
+ */
+
+// Load the shared config builder
+require_once __DIR__ . '/../../../.phan/config.php';
+
+// Generate config for this project
+return make_phan_config(
+	__DIR__ . '/..',
+	array(
+		'directory_list'                  => array(
+			'src',
+			'vendor',
+		),
+		'exclude_analysis_directory_list' => array(
+			'vendor/',
+		),
+	)
+);
