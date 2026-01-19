02008aee63090484c644486ea39deb2c27ed87fb
Igor Zinovyev <zinigor@gmail.com>
2026-01-19 19:21:34 +0300

    Added Redis stubs.

--- .phan/config.php
+++ .phan/config.php
@@ -10,14 +10,15 @@ require_once __DIR__ . '/../../../.phan/config.php';
 
 // Generate config for this project
 return make_phan_config(
-	__DIR__ . '/..',
-	array(
-		'directory_list'                  => array(
-			'src',
-			'vendor',
-		),
-		'exclude_analysis_directory_list' => array(
-			'vendor/',
-		),
-	)
+    __DIR__ . '/..',
+    array(
+        'directory_list'                  => array(
+            'src',
+            'vendor',
+        ),
+        'exclude_analysis_directory_list' => array(
+            'vendor/',
+        ),
+        '+stubs' => array( 'redis' )
+    )
 );
